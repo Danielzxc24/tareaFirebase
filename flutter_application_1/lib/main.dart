a633cec8471b4b94967cdeadc29991527519175a
Danielzxc24 <danielmartinez69207@gmail.com>
2024-06-08 21:56:18 -0600

    actualización del main

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,20 +1,26 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/firebase_options.dart';
+import 'screen/homePage.dart';
 
-void main() {
-  runApp(const MainApp());
+void main() async {
+ WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp( MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      title: 'Gestión de Notas',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      home: homePage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
